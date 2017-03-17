96ac1266c08251863b327169daf9a819a928c20a
qwt311 <qwt311@126.com>
2017-03-17 17:52:30 +0800

    新增log4j配置

---
+++ src/main/java/com/test/quartz/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.test.quartz.controller;
+
+import org.apache.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by qiaowentao on 2017/3/17.
+ */
+@Controller
+//@RequestMapping(value="/user")
+public class UserController {
+
+    private static org.slf4j.Logger LOG = LoggerFactory.getLogger("userController1");
+    private static Logger logger = Logger.getLogger("userController2");
+
+    @RequestMapping(value="/user", method={RequestMethod.GET,RequestMethod.POST})
+    public ModelAndView index(HttpServletRequest request, HttpServletResponse response,ModelAndView modelAndView){
+        LOG.debug("进入userController");
+        logger.debug("进入userController啦啦啦");
+        modelAndView.setViewName("/index");
+        return modelAndView;
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: qiaowentao
+  Date: 2017/3/17
+  Time: 15:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>主页</title>
+</head>
+<body>
+<p2>你好</p2>
+</body>
+</html>
